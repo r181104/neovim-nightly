49633dd44fd367df31bab2c7bcd04c64b85af39a
r181104 <sten181104@gmail.com>
2025-10-18 01:40:16 +0530

    s

---
+++ lua/pack/colors.lua
@@ -0,0 +1,28 @@
+vim.pack.add({
+	{ src = "https://github.com/olimorris/onedarkpro.nvim" },
+})
+
+require("onedarkpro").setup({
+	styles = {
+		types = "bold,italic",
+		methods = "NONE",
+		numbers = "bold",
+		strings = "italic",
+		comments = "italic",
+		keywords = "bold,italic",
+		constants = "bold",
+		functions = "italic",
+		operators = "NONE",
+		variables = "bold",
+		parameters = "NONE",
+		conditionals = "italic",
+		virtual_text = "italic",
+	},
+	highlights = {
+		Comment = { italic = true, underline = true, extend = true },
+		Directory = { bold = true },
+		ErrorMsg = { italic = true, bold = true },
+	},
+})
+
+vim.cmd("colorscheme onedark_vivid")
